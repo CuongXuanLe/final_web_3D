55bc9eba866dd86971fa6429c48c1e526a879a73
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2024-01-12 22:01:18 +0700

    fix backdrop of shirt model

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,9 +1,17 @@
 import React, { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Center, useGLTF, Environment, AccumulativeShadows } from "@react-three/drei";
+import {
+  OrbitControls,
+  Center,
+  useGLTF,
+  Environment,
+  AccumulativeShadows,
+  RandomizedLight,
+} from "@react-three/drei";
 
 export const App = ({ position = [ -1, 0, 2.5 ], fov = 25}) => (
   <Canvas
+    shadows
     eventSource={document.getElementById('root')}
     eventPrefix="client"
     camera={{ position, fov }} >
@@ -43,7 +51,22 @@ function Backdrop () {
       scale={10}
       rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
-    ></AccumulativeShadows>
+    >
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+       <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={0.25}
+        ambient={0.55}
+        position={[5, 5, -9]}
+      />
+    </AccumulativeShadows>
   )
 }
 
