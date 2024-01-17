f2b46f25426f4fde78613c52b5a8a651b4287d32
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2024-01-17 23:41:25 +0700

    update all

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import { App as Canvas } from './Canvas'
-import Overlay from './Overlay'
 
-createRoot(document.getElementById('root')).render(
-  <>
-    <Canvas />
-    <Overlay />
-  </>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
-
